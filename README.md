9967cff3cc3f989700abd2199e10a1b3ac76c50c
Kristian Padilla <ketpadilla05@gmail.com>
2025-02-21 21:08:44 +0800

    UI/UX Implementation 1.00 - Updated app.py to use templates - created /static 	- custom-bootstrap.css for custom bootstrap colors 	- style.css for website colors 	- dark-mode.js for light-dark mode toggle (not being used) - created /templates 	- calories.html 	- icons.html for svg icons used 	- index.html for landing page 	- login.html for login and registration 	- nav.html for navigation bar 	- page_not_found.html for 404 errors

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # cpe106l-4_2T2425-project
 Group 3's Project in partial fulfillments of CPE106L-4 for Term 2 of A.Y.2024-2025
+
+To Fix:
+Implement dark mode toggle
--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, session, url_for, redirect, render_template, request
+from markupsafe import escape
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key' # TODO
+WEB_NAME = 'Trackabite'
 
 @app.route("/")
-def main():
-    return "<p>Hello, World!</p>"
+def index(page_title = WEB_NAME):
+    return render_template('index.html', title = page_title)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['user'] = "test_user"
+        return redirect(url_for('profile', username=session['user']))
+    return render_template('login.html', title="Login") 
+
+@app.route("/logout")
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+  
+@app.route("/user/<username>")
+def profile(username):
+  return f"{escape(username)}\'s Profile"
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return render_template('page_not_found.html', title = '404'), 404
+
+if __name__ == "__main__":
+  app.run(debug=True)
