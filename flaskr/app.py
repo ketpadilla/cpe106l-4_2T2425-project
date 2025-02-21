90152dd3fac54858063e1f325ad33e84e1a9aeed
Kristian Padilla <ketpadilla05@gmail.com>
2025-02-22 02:46:44 +0800

    Integrated UI into partially implemented user management - temporarily ussed routes.py and models.py - added scripts.js and jquery.js - home.html -> registration.html - abstracted login layout in registration.html into login.html - fixed CSS issues in custom-bootstrap.css and style.css (dark mode colors have been set as root colors) - adjusted elements in footer, layout, and nav

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,33 +1,43 @@
-from flask import Flask, session, url_for, redirect, render_template, request
-from markupsafe import escape
+from flask import Flask, render_template, session, redirect, request, url_for
+from functools import wraps
+import pymongo
+from .user.models import User 
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key' # TODO
 WEB_NAME = 'Trackabite'
 
+client = pymongo.MongoClient('localhost', 27017)
+db = client.user_login_system
+
 @app.route("/")
-def index(page_title = WEB_NAME):
-    return render_template('index.html', title = page_title)
+def index(page_title=WEB_NAME):
+    return render_template('index.html', title=page_title)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         session['user'] = "test_user"
         return redirect(url_for('profile', username=session['user']))
-    return render_template('login.html', title="Login") 
+    return render_template('login.html', title="Login")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        return User().signup()
+    return render_template('register.html', title="Register")
 
 @app.route("/logout")
 def logout():
     session.pop('user', None)
     return redirect(url_for('login'))
-  
+
 @app.route("/user/<username>")
 def profile(username):
-  return f"{escape(username)}\'s Profile"
+    return f"{escape(username)}'s Profile"
 
 @app.errorhandler(404)
 def page_not_found(error):
-  return render_template('page_not_found.html', title = '404'), 404
+    return render_template('page_not_found.html', title='404'), 404
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
