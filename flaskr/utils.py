2763aa20a1c77955b5905e424ed84730a6ce6291
Rhodave Carl Otayde <rcuotayde@mymail.mapua.edu.ph>
2025-03-12 23:01:57 +0800

    Update utils.py

--- flaskr/utils.py
+++ flaskr/utils.py
@@ -2,15 +2,30 @@ from flask import Flask, session, redirect
 from functools import wraps
 
 def login_required(f):
-  @wraps(f)
-  def wrap(*args, **kwargs):
-    if 'logged_in' in session:
-      return f(*args, **kwargs)
-    else: 
-      print('Not logged in. Returning to Home Page')
-      return redirect('/')
+    """Decorator to ensure that a user is logged in before accessing a route.
 
-  return wrap
+    This decorator checks if the 'logged_in' key is present in the session. If the user
+    is not logged in, they are redirected to the home page.
+
+    Args:
+        f (function): The route function to be wrapped.
+
+    Returns:
+        function: The wrapped function that enforces login requirements.
+    """
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            print('Not logged in. Returning to Home Page')
+            return redirect('/')
+
+    return wrap
 
 def debug_border():
-  print("-" * 50)
+    """Print a debug border to the console.
+
+    This function prints a line of dashes to visually separate debug messages in the console.
+    """
+    print("-" * 50)
